d47de0f3f7de84a928015daf34084467a20ceb8b
shanexi <shane@myshell.ai>
2024-10-10 09:49:07 +0800

    UI ref select

--- src/fabritor/UI/setter/RefSelectFormItem.tsx
+++ src/fabritor/UI/setter/RefSelectFormItem.tsx
@@ -1,11 +1,53 @@
 import { Flex } from "react-system";
 import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
-import { Form, Select } from "antd";
+import { Dropdown, Form, type MenuProps, Select } from "antd";
 import React from "react";
+import { MenuInfo } from "rc-menu/lib/interface";
+
+const items: MenuProps['items'] = [
+  {
+    key: 'current',
+    type: 'group',
+    label: 'current',
+    children: [
+      {
+        key: 'Task',
+        label: 'Task',
+        children: [
+          {
+            key: 'Image Canvas',
+            label: 'Image Canvas',
+            children: [
+              {
+                key: 'url',
+                label: 'url'
+              }
+            ]
+          }
+        ]
+      },
+    ],
+  },
+  {
+    key: 'global',
+    type: 'group',
+    label: 'global',
+    children: [
+      {
+        key: 'Start Context',
+        label: 'Start Context',
+        children: []
+      },
+    ],
+  },
+];
 
 export function RefSelectFormItem(props: {
   name: string;
+  value: string;
+  onChange: (value: string[], selectedOptions: unknown[]) => void;
 }) {
+  const [keyPath, setKeyPath] = React.useState<string[]>([]);
   return (
     <Form.Item
       name={props.name}
@@ -14,7 +56,28 @@ export function RefSelectFormItem(props: {
         marginRight: 2
       }}/>Ref</Flex>}
     >
-      <Select options={[]}/>
+      <Dropdown menu={
+        {
+          onClick: (info: MenuInfo) => {
+            setKeyPath(info.keyPath)
+          },
+          selectedKeys: keyPath,
+          items
+        }
+      }
+                placement="bottomRight"
+                overlayStyle={{
+                  zIndex: 100, // antd dropdown cascade bug, sub menu z-index = 100
+                }}>
+        <Select options={[]}
+          // use Dropdown menus
+                dropdownRender={(originNode) => null}
+                dropdownStyle={{ display: 'none' }}
+                value={keyPath.length === 0 ? null : keyPath.join('/')}
+                placeholder="Please select variable"
+                onClear={() => setKeyPath([])}
+                allowClear/>
+      </Dropdown>
     </Form.Item>
   )
 }
