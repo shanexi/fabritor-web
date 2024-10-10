4ba8dc46c733935af28963a610c3ce71d845b58c
shanexi <shane@myshell.ai>
2024-10-10 15:47:40 +0800

    Transform external variables

--- src/fabritor/image-canvas.model.ts
+++ src/fabritor/image-canvas.model.ts
@@ -1,61 +1,23 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 import { type MenuProps } from "antd";
 import { injectable, preDestroy } from "inversify";
 import { Editor } from "../editor";
-import mitt from 'mitt';
-
-const VARIABLES = [
-  {
-    key: 'current',
-    type: 'group',
-    label: 'current',
-    children: [
-      {
-        key: 'Task',
-        label: 'Task',
-        children: [
-          {
-            key: 'Image Canvas',
-            label: 'Image Canvas',
-            children: [
-              {
-                key: 'url',
-                label: 'url'
-              }
-            ]
-          }
-        ]
-      },
-    ],
-  },
-  {
-    key: 'global',
-    type: 'group',
-    label: 'global',
-    children: [
-      {
-        key: 'Start Context',
-        label: 'Start Context',
-        children: []
-      },
-    ],
-  },
-]
+import mitt from "mitt";
 
 @injectable()
 export class ImageCanvasModel {
   emitter = mitt<{
-    'loadFromJSON': unknown;
+    loadFromJSON: unknown;
   }>();
 
-  @observable variables: MenuProps['items'] = VARIABLES;
+  @observable variables: MenuProps["items"] = [];
   private isEditorReadyPromiseResolve: ((value: unknown) => void) | undefined;
   private isEditorReadyPromise: Promise<unknown>;
   private editor: Editor;
 
   constructor() {
-    makeObservable(this)
-    this.isEditorReadyPromise = new Promise(resolve => {
+    makeObservable(this);
+    this.isEditorReadyPromise = new Promise((resolve) => {
       this.isEditorReadyPromiseResolve = resolve;
     });
   }
@@ -67,16 +29,81 @@ export class ImageCanvasModel {
 
   setEditor(editor: Editor) {
     this.editor = editor;
-    this.isEditorReadyPromiseResolve('');
+    this.isEditorReadyPromiseResolve("");
   }
 
   async canvas2Json(): Promise<unknown> {
-    await this.isEditorReadyPromise
-    return this.editor.canvas2Json()
+    await this.isEditorReadyPromise;
+    return this.editor.canvas2Json();
   }
 
   async loadFromJSON(json: unknown): Promise<void> {
-    await this.isEditorReadyPromise
-    this.emitter.emit('loadFromJSON', json);
+    await this.isEditorReadyPromise;
+    this.emitter.emit("loadFromJSON", json);
+  }
+
+  @action.bound
+  setVariables(variables: any[]) {
+    this.variables = transformVariables(variables);
   }
 }
+
+// todo: add Test
+/*
+[
+    {
+        "label": "current",
+        "children": []
+    },
+    {
+        "label": "global",
+        "children": [
+            {
+                "label": "Start-Context",
+                "value": "{{}}",
+                "children": [
+                    {
+                        "label": "config",
+                        "value": "{{__context__key_1728467722718__}}"
+                    }
+                ]
+            }
+        ]
+    }
+]
+    
+[
+    {
+        "label": "current",
+        "key": "current",
+        "type": "group",
+        "children": []
+    },
+    {
+        "label": "global",
+        "key": "global",
+        "type": "group",
+        "children": [
+            {
+                "label": "Start-Context",
+                "key": "{{}}",
+                "children": [
+                    {
+                        "label": "config",
+                        "key": "{{__context__key_1728467722718__}}"
+                    }
+                ]
+            }
+        ]
+    }
+]
+*/
+function transformVariables(source: any[]): MenuProps["items"] {
+  return source.map((item) => ({
+    label: item.label,
+    key: item.value || item.label,
+    type: item.value == null ? "group" : undefined,
+    children: item.children ? transformVariables(item.children) : undefined,
+  }));
+}
+
--- src/stories/Canvas.stories.tsx
+++ src/stories/Canvas.stories.tsx
@@ -1,28 +1,53 @@
-import '../index.css';
-import 'reflect-metadata';
-import { Meta, StoryFn, StoryObj } from '@storybook/react';
-import { Container, interfaces } from 'inversify';
-import { Provider } from 'inversify-react';
+import "../index.css";
+import "reflect-metadata";
+import { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { Container, interfaces } from "inversify";
+import { Provider } from "inversify-react";
 import { ImageCanvasModel } from "../fabritor/image-canvas.model";
 import { ImageCanvas } from "../fabritor";
 
 const container = new Container();
-container.bind<ImageCanvasModel>('ImageCanvasModel').to(ImageCanvasModel).inSingletonScope();
+container
+  .bind<ImageCanvasModel>("ImageCanvasModel")
+  .to(ImageCanvasModel)
+  .inSingletonScope();
+const model = container.get<ImageCanvasModel>("ImageCanvasModel");
+model.setVariables([
+  {
+    label: "current",
+    children: [],
+  },
+  {
+    label: "global",
+    children: [
+      {
+        label: "Start-Context",
+        value: "{{}}",
+        children: [
+          {
+            label: "config",
+            value: "{{__context__key_1728467722718__}}",
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-const withContainer =
-  (container: interfaces.Container) => (Story: StoryFn) => (
+const withContainer = (container: interfaces.Container) => (Story: StoryFn) =>
+  (
     <Provider container={container}>
-      <Story/>
+      <Story />
     </Provider>
   );
 
 const meta = {
-  title: 'Canvas',
+  title: "Canvas",
   component: ImageCanvas,
   decorators: [withContainer(container)],
   parameters: {
-    layout: 'fullscreen',
-  }
+    layout: "fullscreen",
+  },
 } satisfies Meta<typeof ImageCanvas>;
 
 export default meta;
@@ -32,6 +57,6 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Canvas',
+    label: "Canvas",
   },
 };
