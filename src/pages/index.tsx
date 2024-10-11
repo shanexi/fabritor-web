f00a2c24be31889d8841f3c90e1e4ac05834bf56
shanexi <shane@myshell.ai>
2024-10-11 10:17:53 +0800

    Fix site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import "reflect-metadata";
+
 import { definePageConfig } from "../../.ice";
 import { ImageCanvas } from "../fabritor";
 import '../index.css';
+import { Provider } from "inversify-react";
+import { Container } from "inversify";
+import { ImageCanvasModel } from "../fabritor/image-canvas.model";
 
 export const pageConfig = definePageConfig(() => ({
   title: 'fabritor, A creative editor based on fabricjs.'
 }));
 
+const container = new Container();
+container
+  .bind<ImageCanvasModel>("ImageCanvasModel")
+  .to(ImageCanvasModel)
+  .inSingletonScope();
+
 export default function () {
-  return <ImageCanvas/>;
+  return <Provider container={container}>
+    <ImageCanvas/>
+  </Provider>;
 }
