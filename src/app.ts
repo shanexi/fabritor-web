b510c9ee286fdcc231388513f5c319b3dd78eea9
shanexi <shane@myshell.ai>
2024-11-14 16:37:51 +0800

    Build error comment font.css

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { defineAppConfig } from "../.ice";
 import './i18n';
 
-import './font.css';
+// import './font.css';
 
 // App config, see https://v3.ice.work/docs/guide/basic/app
 export default defineAppConfig(() => ({}));
